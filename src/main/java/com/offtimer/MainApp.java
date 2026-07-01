54fb9d2eeffe8cecd4ece7a97f4a98d2ea346143
bloodSUCKER <maksimchikGit@gmail.com>
2026-07-01 20:30:41 +0300

    Указать BloodSUCKER как издателя в метаданных установщика

--- src/main/java/com/offtimer/MainApp.java
+++ src/main/java/com/offtimer/MainApp.java
@@ -17,7 +17,7 @@ import java.io.InputStream;
 
 public class MainApp {
 
-    private static final String VERSION = "1.0.3";
+    private static final String VERSION = "1.0.4";
 
     private JFrame frame;
     private JComboBox<ActionType> actionCombo;
