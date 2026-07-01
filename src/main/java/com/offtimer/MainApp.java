3c521b68f7b684ea482e8caaf85f941d8aff514f
bloodSUCKER <maksimchikGit@gmail.com>
2026-07-01 21:59:30 +0300

    v1.0.19: убрать rcedit, ломавший публикацию релиза

--- src/main/java/com/offtimer/MainApp.java
+++ src/main/java/com/offtimer/MainApp.java
@@ -16,7 +16,7 @@ import java.awt.event.WindowEvent;
 
 public class MainApp {
 
-    private static final String VERSION = "1.0.18";
+    private static final String VERSION = "1.0.19";
 
     private JFrame frame;
     private JComboBox<ActionType> actionCombo;
